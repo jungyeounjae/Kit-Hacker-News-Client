988b57cfbbd21f832cf1d7eb30481e70b6f604e4
yeounjae <duswp220@gmail.com>
2021-07-18 15:10:30 +0900

    show news list via hash

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -118,24 +118,41 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"app.js":[function(require,module,exports) {
-// let ajax = new XMLHttpRequest(); 変数
+var container = document.getElementById('root'); // let ajax = new XMLHttpRequest(); 変数
+
 var ajax = new XMLHttpRequest(); // const data
 
+var content = document.createElement('div');
 var NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
+var CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 ajax.open('GET', NEWS_URL, false);
 ajax.send();
 var newsFeed = JSON.parse(ajax.response);
 var ul = document.createElement('ul');
+window.addEventListener('hashchange', function () {
+  console.log(location.hash);
+  var id = location.hash.substring(1);
+  ajax.open('GET', CONTENT_URL.replace('@id', id), false);
+  ajax.send();
+  var newsContent = JSON.parse(ajax.response);
+  var title = document.createElement('h1');
+  title.innerHTML = newsContent.title;
+  content.appendChild(title);
+  console.log(newsContent);
+});
 
 for (var i = 0; i < 10; i++) {
   var li = document.createElement('li'); //
 
-  li.innerHTML = newsFeed[i].title; //<li>${newsFeed[i].title}</li>
-
+  var a = document.createElement('a');
+  a.href = "#".concat(newsFeed[i].id);
+  a.innerHTML = "".concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")");
+  li.appendChild(a);
   ul.appendChild(li);
 }
 
-document.getElementById('root').appendChild(ul);
+container.appendChild(ul);
+container.appendChild(content);
 },{}],"../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -164,7 +181,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64411" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53628" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
